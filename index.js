df1163d2ff1f50f9bf3f7c3b5bd928d5b2fd7e27
ambarish2012 <ambarish@shippable.com>
2017-10-05 11:35:15 -0700

    update sample

---
+++ index.js
@@ -0,0 +1,22 @@
+var knex = require('knex');
+
+var db = knex.initialize({
+  client: 'postgresql',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'shippable',
+    password : '',
+    database : 'test'
+  }
+});
+
+db.schema.hasTable('things').then(function (exists) {
+  if (!exists) {
+    console.log('Table does not exist, creating...');
+    db.raw('CREATE TABLE things (name char(20));').then(function (res) {
+      console.dir(res);
+    });
+  }
+});
+
+module.exports.db = db;
