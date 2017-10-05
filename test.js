cc1e4732411f37a3ed2e42c617f986c7b5717f47
ambarish2012 <ambarish@shippable.com>
2017-10-05 14:36:47 -0700

    update sample

--- test.js
+++ test.js
@@ -12,4 +12,9 @@ describe('Migration tests', function () {
       expect(exists).to.equal(true);
     });
   });
+  it('Check if the article table has the primary KPqdQhxbPa994hnzjcE2VqQpDslf55723cKPUOGSmMY3g', function () {
+    db('pg_catalog.pg_indexes').where({tablename: 'article'}).select('indexname').then(function (count) {
+      expect(count).to.equal('article_pkey');
+    });
+  });
 });
