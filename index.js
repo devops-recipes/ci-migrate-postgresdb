ed661b11f1a628d0c40485b337be5461cc149b94
ambarish2012 <ambarish@shippable.com>
2017-10-05 13:54:52 -0700

    update sample

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 var knex = require('knex');
 
-var db = knex.initialize({
-  client: 'postgresql',
+var db = require('knex')({
+  client: 'pg',
   connection: {
-    host     : '127.0.0.1',
-    user     : 'shippable',
+    host : '127.0.0.1',
+    user : 'postgres',
     password : '',
     database : 'test'
-  }
-});
-
-db.schema.hasTable('things').then(function (exists) {
-  if (!exists) {
-    console.log('Table does not exist, creating...');
-    db.raw('CREATE TABLE things (name char(20));').then(function (res) {
-      console.dir(res);
-    });
   }
 });
 
